f988599c25040808e6067bdf715ca9c59d3543e8
syoon <asn6878@gmail.com>
2021-09-02 14:24:10 +0000

    practice

---
+++ board/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('viewjson/', views.viewjson, name='viewjson'),
+    path('boardlist/', views.boardList, name='boardlist'),
+]
